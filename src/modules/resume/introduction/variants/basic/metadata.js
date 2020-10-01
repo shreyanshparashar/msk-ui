6b1058c15edf02d5c6d2637517b2e229c97d2d26
shreyanshparashar <62754950+shreyanshparashar@users.noreply.github.com>
2020-10-02 00:51:46 +0530

    Update metadata.js

--- src/modules/resume/introduction/variants/basic/metadata.js
+++ src/modules/resume/introduction/variants/basic/metadata.js
@@ -2,7 +2,7 @@
 const metadata = {
   id: "resume-introduction-basic",
   name: "Basic",
-  description: "A very basic variant",
+  description: "A very basic variant for basic touch.",
 }
 
 export default metadata;
